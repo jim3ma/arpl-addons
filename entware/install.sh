d92c5f794f96512f04ca8272bb8abb35f726fe04
Jim Ma <majinjing3@gmail.com>
2023-05-01 12:30:22 +0800

    add entware

---
+++ entware/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env ash
+
+if [ "${1}" = "late" ]; then
+  echo "Copying EntWare to HD"
+  if [ -e /tmpRoot/opt ]; then
+    echo /tmpRoot/opt exists, skip copy EntWare files
+    exit 0
+  fi
+  cp -vfr /opt /tmpRoot/opt
+  if grep -qF /opt/etc/profile /tmpRoot/etc/profile; then
+	  echo "Confirmed: Entware Profile in Global Profile"
+  else
+	  echo "Adding: Entware Profile in Global Profile"
+    cat >> /tmpRoot/etc/profile <<"EOF"
+
+# Load Entware Profile
+[ -r "/opt/etc/profile" ] && . /opt/etc/profile
+EOF
+  fi
+fi
