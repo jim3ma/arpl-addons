b557546f8b0ee91a7d114ef230a6dc4fb0e390ab
Jim Ma <majinjing3@gmail.com>
2023-05-01 12:36:54 +0800

    update entware

--- entware/install.sh
+++ entware/install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env ash
 
+if [ "${1}" = "early" ]; then
+  if grep -qF /opt/etc/profile /etc/profile; then
+	  echo "Confirmed: Entware Profile in Global Profile"
+  else
+	  echo "Adding: Entware Profile in Global Profile"
+    cat >> /etc/profile <<"EOF"
+
+# Load Entware Profile
+[ -r "/opt/etc/profile" ] && . /opt/etc/profile
+EOF
+  fi
+fi
+
 if [ "${1}" = "late" ]; then
   echo "Copying EntWare to HD"
   if [ -e /tmpRoot/opt ]; then
